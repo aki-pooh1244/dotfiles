1ab4c78c9aca91d4a90733d74f8f2179f032ce18
aki-pooh1244 <aki.pooh1244@gamil.com>
2022-04-28 14:21:45 +0900

    Edit in fedora

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -674,7 +674,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme)))
+ '(package-selected-packages '(zenburn-theme))
+ '(zoom-ignore-predicates
+   '((lambda nil
+       (>
+        (count-lines
+         (point-min)
+         (point-max))
+        20))))
+ '(zoom-ignored-buffer-name-regexps '("^*calc"))
+ '(zoom-ignored-major-modes '(dired-mode markdown-mode))
+ '(zoom-size '(0.618 . 0.618)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
