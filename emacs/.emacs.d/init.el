8dfbb2577465a462a7d81cc0216d086fc284b7b1
aki-pooh1244 <aki.pooh1244@gmail.com>
2024-01-11 12:51:13 +0900

    ADD: switch-window config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -374,9 +374,12 @@
              "C-x 0" 'switch-window-then-delete
              "C-x 4 d" 'switch-window-then-dired
              "C-x 4 f" 'switch-window-then-find-file
+             "C-x 4 r" 'switch-window-then-find-file-read-only
              "C-x 4 C-o" 'switch-window-then-display-buffer
              "C-x 4 0" 'switch-window-then-kill-buffer)
-  (setq switch-window-shortcut-style 'qwerty))
+  (setq switch-window-shortcut-style 'qwerty
+        switch-window-auto-resize-window t
+        switch-window-mouse-mode))
 
 ;; (setup-include "ace-window"
 ;;   :preapre (setup-keybinds nil
