f964fcf204e6aa402a2d0db341371c98e96939ea
aki-pooh1244 <aki.pooh1244@gmail.com>
2024-01-19 15:56:35 +0900

    Auto add

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1174,7 +1174,7 @@ ay-fill-column-indicator"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(compat zenburn-theme))
+ '(package-selected-packages '(csv-mode compat zenburn-theme))
  '(warning-suppress-log-types '((modus-themes)))
  '(zoom-ignore-predicates
    '((lambda nil
