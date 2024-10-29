3656ab5042b9c1a75b36efd66d8c2b4d2d32a2ca
aki-pooh1244 <aki.pooh1244@gmail.com>
2024-10-29 14:47:31 +0900

    CHG: rewrite early-init.el

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -1,35 +1,30 @@
-;;; early-init.el -- early configuration
+;;; early-init.el -- early initialization tweaks -*- lexical-binding: t; -*-
 
-;; -*- lexical-binding: t -*-
-;; -*- no-byte-compile: t -*-
+;;; Commentary:
 
 ;;; Code:
+(setq load-prefer-newer t
+      package-enable-at-startup nil
+      inhibit-startup-screen t
+      initial-buffer-choice nil
+      initial-scratch-message nil
+      initial-major-mode 'fundamental-mode
+      ;; UI
+      default-frame-alist '((tool-bar-lines . 0)
+		                    (menu-bar-lines . 0)
+		                    (vertical-scroll-bars)
+		                    (width . 100)
+		                    (height . 55)
+		                    ;; (fullscreen . maximized)
+		                    ))
 
+(setq gc-cons-threshold (* 10 128 1024 1024)
+      garbage-collection-message nil)
+(setq read-process-output-max (* 8 1024 1024))
 
-(setq load-prefer-newer t)
+(setq byte-compile-warnings '(not obsolete)
+      warning-suppress-log-types '((comp) (bytecomp))
+      native-comp-async-report-warnings-errors 'silent)
 
-(let ((dir (file-name-directory (or load-file-name buffer-file-name))))
-  (add-to-list 'load-path (expand-file-name "site-lisp" dir))
-  (add-to-list 'load-path (expand-file-name "el-get" dir)))
+;;; early-init.el ends here.
 
-(setq package-enable-at-startup nil)
-
-;; (with-eval-after-load 'package
-;;   (add-to-list 'package-archives
-;;                (cons "melpa" "https://melpa.org/packages/")
-;;                t)
-;;   (add-to-list 'package-archives
-;;                (cons "org" "https://orgmode.org/elpa/")
-;;                t))
-;; (setq initial-frame-alist '((fullscreen . maximized)))
-(setq initial-frame-alist '((width . 100)
-                            (height . 60)))
-;; (custom-set-variables '(initial-frame-alist '((fullscreen . maximized))))
-;; (setq default-frame-alist
-;;       '((width . 90)
-;;         (height . 50)))
-(setq default-frame-alist '((width . 100)
-                            (height . 60)))
-
-;; End:
-;;; early-init.el ends here
